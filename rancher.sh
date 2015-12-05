2a14aa9b4ebd3487b176d68b0411b56c7a686d77
Vincent Boutour <vincent.boutour@gmail.com>
2015-12-05 15:33:29 +0000

    Adding label for Traefik

--- rancher.sh
+++ rancher.sh
@@ -3,6 +3,8 @@
 docker run -d \
   --name rancher \
   -p 8080:8080 \
+  --label traefik.port=8080 \
+  --label traefik.frontend.passHostHeader=true \
   -v /var/rancher/mysql:/var/lib/mysql \
   -v /var/rancher/cattle:/var/lib/cattle \
   -m 1G \
