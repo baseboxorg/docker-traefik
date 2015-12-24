ebf03119d07d0162366387966004fe59c5a46837
Vincent Boutour <ViBiOh@users.noreply.github.com>
2015-12-24 10:59:35 +0100

    Update rabbit.sh

--- rabbit.sh
+++ rabbit.sh
@@ -4,7 +4,7 @@ docker run -d \
   --name rabbit \
   --hostname rabbitmq \
   -p 5672:5672 \
-  --label traefik.port=15672 \
-  --label traffic.frontend.passHostHeader=true \
+  -l traefik.port=15672 \
+  -l traffic.frontend.passHostHeader=true \
   rabbitmq:3-management \
   sh -c 'echo "[{rabbit, [{loopback_users, []}, {default_vhost,<<\"rabbit\">>}]}]." > /etc/rabbitmq/rabbitmq.config && rabbitmq-server'`
